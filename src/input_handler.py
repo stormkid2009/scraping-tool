ab5da408eff8d77635b66c4ad9eedf5064726c11
Anwar Ahmed <nileking2007@gmail.com>
2024-08-28 03:12:30 +0300

    adding input handler

---
+++ src/input_handler.py
@@ -0,0 +1,8 @@
+# src/input_handler.py
+
+def get_user_url():
+    """
+    Prompts the user to enter a URL and returns it.
+    """
+    url = input("Please enter the URL to scrape: ")
+    return url
--- src/scraper.py
+++ src/scraper.py
@@ -1,8 +1,15 @@
+# web_scraping_project/scraper.py
+
 import requests
 from bs4 import BeautifulSoup
+from .input_handler import get_user_url  # Import the function from the module
 
-url = "https://thegreatdevices.com/product/%d9%85%d9%83%d9%8a%d9%81-%d8%b3%d8%a8%d9%84%d8%aa-%d8%aa%d9%84%d9%8a%d8%b2%d9%88%d9%86-22000-%d9%88%d8%ad%d8%af%d8%a9-%d8%b1%d9%88%d8%aa%d8%a7%d8%b1%d9%8a-%d8%a8%d8%a7%d8%b1%d8%af-%d9%88-%d8%ad/"
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
+def scrape():
+    url = get_user_url()  # Get the URL from the user
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'lxml')
 
-print(soup.title.text)
+    print(soup.title.text)
+
+if __name__ == "__main__":
+    scrape()
